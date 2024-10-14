c99c87e8ddc3cce0eac9563998b8ae2074ccca7d
Enab13d <extendeddirection@gmail.com>
2024-10-14 07:14:47 +0300

    added route transitions and page titles

--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,25 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: "Home"
+      }
     }, {
       path: '/weather/:state/:city',
       name: 'cityView',
       component: CityView,
-      
+      meta: {
+        title: "Weather"
+      }
 
     }
   ]
+})
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  document.title = `${to.params.state ? `${to.params.city}, ${to.params.state}`: to.meta.title} | The local weather`;
+  next()
 })
 
 export default router
